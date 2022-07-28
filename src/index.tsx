f7152300b2e690a085a9093afe9331a834bc03b9
Sangji Lee <sangllee20@gmail.com>
2022-07-28 12:55:25 +0900

    polish: add index.scss and RecoilRoot to index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import Routes from "./router/Routes";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Routes from './router/Routes';
+import { RecoilRoot } from 'recoil';
+import './styles/index.scss';
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+  document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Routes />
+    <RecoilRoot>
+      <Routes />
+    </RecoilRoot>
   </React.StrictMode>
 );
