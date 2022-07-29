91121eee28c3f007d2f3fe567641f6a183f47970
Sangji Lee <sangllee20@gmail.com>
2022-07-29 12:32:17 +0900

    Revert "remove: unused files and comments"
    This reverts commit 361cfbfd5717682bcb971b1dec2c99437f54f47d.

--- src/pages/ViewClass.tsx
+++ src/pages/ViewClass.tsx
@@ -1,21 +1,25 @@
-import React from 'react';
-import Header from '../components/layout/Header';
-import Layout from '../components/layout/Layout';
-import Table from '../components/schedule/table/ScheduleTable';
-import { ScheduleProps } from '../types/schedule';
-import { Link } from 'react-router-dom';
-import '../style/table.scss';
+import React from "react";
+import Header from "../components/layout/Header";
+import Layout from "../components/layout/Layout";
+import Table from "../components/schedule/table/ScheduleTable";
+import { useScheduleModel } from "../api/model/useScheduleModels";
+import { ScheduleProps } from "../types/schedule";
+import { Link } from "react-router-dom";
+import "../style/table.scss";
 
 const ViewClass = () => {
-  // const [data, setData] = React.useState<ScheduleProps[] | any>();
-  // console.log(data?.monday);
-
+  const { getSchedule } = useScheduleModel();
+  const [data, setData] = React.useState<ScheduleProps[] | any>();
+  console.log(data?.monday);
+  React.useEffect(() => {
+    getSchedule().then((response) => setData(response));
+  }, []);
   return (
     <>
       <Layout>
-        <div className='title'>
+        <div className="title">
           <h2>Class schedule</h2>
-          <Link to='/add' className='btn btn-type1'>
+          <Link to="/add" className="btn btn-type1">
             Add Class Schedule
           </Link>
         </div>
---
+++ 🗑/queries/queryRequest.ts
@@ -0,0 +1,5 @@
+import { useQuery, useMutation, QueryClient } from 'react-query';
+import { get, post, put, _delete } from './httpRequest';
+import { scheduleInfo } from './services';
+
+// export const getSchedule(){}
