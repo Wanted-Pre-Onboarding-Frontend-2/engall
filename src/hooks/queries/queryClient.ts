fe7e4890c6be571178c46f0bee80db1a97843149
Sangji Lee <sangllee20@gmail.com>
2022-08-28 20:28:20 +0900

    Refactor : create & migarte queryClient file

---
+++ src/hooks/queries/queryClient.ts
@@ -0,0 +1,10 @@
+import { QueryClient } from 'react-query';
+
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      suspense: true,
+      staleTime: 1000 * 60,
+    },
+  },
+});
--- src/index.tsx
+++ src/index.tsx
@@ -2,28 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Routes from './router/Routes';
 import { RecoilRoot } from 'recoil';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import './style/index.scss';
+import { QueryClientProvider } from 'react-query';
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      suspense: true,
-      staleTime: 1000 * 60,
-    },
-  },
-});
+import { ReactQueryDevtools } from 'react-query/devtools';
+import './style/index.scss';
+import { queryClient } from './hooks/queries/queryClient';
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <ReactQueryDevtools initialIsOpen={false} />
       <RecoilRoot>
         <Routes />
       </RecoilRoot>
+      <ReactQueryDevtools />
     </QueryClientProvider>
   </React.StrictMode>
 );
