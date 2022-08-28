5728913fb679864dd6666895581bb04b1c50b146
Sangji Lee <sangllee20@gmail.com>
2022-08-28 20:43:51 +0900

    Refactor : apply useGetSchedule hook and useAddSchedule hook

--- src/components/schedule/form/AddForm.tsx
+++ src/components/schedule/form/AddForm.tsx
@@ -1,16 +1,18 @@
-import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
-import React, { FormEvent, ChangeEvent, useState } from "react";
-import { Schedule, ScheduleTypes } from "../../../types/schedule";
-import { createSchedule, getSchedule } from "../../../api/httpRequest";
-import { add, format } from "date-fns";
-import { HOURS, MINUTES, MERIDIEMS, daysOfWeek } from "../../../utils/getDate";
-import { useNavigate } from "react-router-dom";
-import Popup from "../../layout/Popup";
-import { submitTime } from "../../../hooks/useDate";
-import { duplicateTime } from "../../../hooks/useDuplicateTime";
+import { FormEvent, ChangeEvent, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { ScheduleTypes } from '../../../types/schedule';
+import { format } from 'date-fns';
+
+import Popup from '../../layout/Popup';
+import { HOURS, MINUTES, MERIDIEMS, daysOfWeek } from '../../../utils/getDate';
+
+import { submitTime } from '../../../hooks/useDate';
+import { duplicateTime } from '../../../hooks/useDuplicateTime';
+import { useAddSchedule } from '../../../hooks/queries/useAddSchedule';
+import { useGetSchedule } from '../../../hooks/queries/useGetSchedule';
 
 const date = new Date();
-const today = format(date, "yyyy-MM-dd");
+const today = format(date, 'yyyy-MM-dd');
 
 interface TimeTypes {
   hour: string;
@@ -19,22 +21,23 @@ interface TimeTypes {
 }
 
 const AddForm = () => {
-  const { data } = useQuery<Schedule[]>(["schedule"], () => getSchedule());
-
-  const queryClient = useQueryClient();
   const navigate = useNavigate();
 
+  // const { data } = useQuery('schedule', () => getSchedule());
+  const addSchedule = useAddSchedule();
+  const schedules = useGetSchedule();
+
   const [popupOpen, setPopupOpen] = useState(false);
+  const [days, setDays] = useState<string[]>([]);
+  const [times, setTimes] = useState<TimeTypes>({
+    hour: '00',
+    minute: '00',
+    meridiem: 'am',
+  });
+
   const popupHandler = (status: boolean) => {
     setPopupOpen(status);
   };
-
-  const [days, setDays] = useState<string[]>([]);
-  const [times, setTimes] = useState<TimeTypes>({
-    hour: "00",
-    minute: "00",
-    meridiem: "am",
-  });
 
   const handleChange = (
     event: ChangeEvent<HTMLInputElement | HTMLSelectElement>
@@ -55,13 +58,6 @@ const AddForm = () => {
     }
   };
 
-  const { mutate } = useMutation(createSchedule, {
-    onMutate: (variables) => {},
-    onSuccess: (data) => {
-      queryClient.invalidateQueries(["schedule"]);
-    },
-  });
-
   const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
@@ -76,16 +72,16 @@ const AddForm = () => {
         end: `${endTime} ${times.meridiem}`,
       };
 
-      const yoilData: any = data?.filter(
+      const yoilData: any = schedules?.filter(
         (day: any) => day.day === newSchedule.day
       );
 
       const isDuplicate: boolean = duplicateTime(newSchedule, yoilData);
 
       if (isDuplicate) {
-        mutate(newSchedule);
-        alert("일정이 추가되었습니다.");
-        navigate("/view");
+        addSchedule(newSchedule);
+        alert('일정이 추가되었습니다.');
+        navigate('/view');
       } else {
         setPopupOpen(true);
       }
@@ -93,43 +89,43 @@ const AddForm = () => {
   };
 
   return (
-    <form method="post" onSubmit={handleSubmit}>
-      <div className="content-wrap">
-        <div className="form-list">
+    <form method='post' onSubmit={handleSubmit}>
+      <div className='content-wrap'>
+        <div className='form-list'>
           <h3>Start Time</h3>
-          <div className="form-start">
-            <div className="form-select">
+          <div className='form-start'>
+            <div className='form-select'>
               <select
-                name="hour"
-                id="hour"
+                name='hour'
+                id='hour'
                 onChange={handleChange}
                 value={times.hour}
               >
                 {HOURS.map((hour: string, index: number) => (
-                  <option value={hour} key={hour + "-" + index}>
+                  <option value={hour} key={hour + '-' + index}>
                     {hour}
                   </option>
                 ))}
               </select>
               <span>:</span>
               <select
-                name="minute"
-                id="minute"
+                name='minute'
+                id='minute'
                 onChange={handleChange}
                 value={times.minute}
               >
                 {MINUTES.map((minute: string, index) => (
-                  <option value={minute} key={minute + "-" + index}>
+                  <option value={minute} key={minute + '-' + index}>
                     {minute}
                   </option>
                 ))}
               </select>
             </div>
             {MERIDIEMS.map((meridiem) => (
-              <div className="form-radio" key={meridiem}>
+              <div className='form-radio' key={meridiem}>
                 <input
-                  type="radio"
-                  name="meridiem"
+                  type='radio'
+                  name='meridiem'
                   id={meridiem}
                   onChange={handleChange}
                   value={meridiem}
@@ -139,14 +135,14 @@ const AddForm = () => {
             ))}
           </div>
         </div>
-        <div className="form-list">
+        <div className='form-list'>
           <h3>Repeat on</h3>
-          <div className="form-repeat">
+          <div className='form-repeat'>
             {daysOfWeek.map((day) => (
-              <div className="form-check" key={day}>
+              <div className='form-check' key={day}>
                 <input
-                  type="checkbox"
-                  name="days"
+                  type='checkbox'
+                  name='days'
                   id={day}
                   value={day}
                   onChange={handleChangeCheckbox}
@@ -157,11 +153,11 @@ const AddForm = () => {
           </div>
         </div>
       </div>
-      <button type="submit" className="btn btn-type1 large form-btn">
+      <button type='submit' className='btn btn-type1 large form-btn'>
         Save
       </button>
       <Popup
-        text="중복된 시간입니다."
+        text='중복된 시간입니다.'
         open={popupOpen}
         onCloseHandler={() => popupHandler(false)}
       />
